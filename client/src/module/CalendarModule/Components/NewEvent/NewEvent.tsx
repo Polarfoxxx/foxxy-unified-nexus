6e3bb5e0e51a2541abbd65f24607c1cf029da208
Michal Suchovsky <99112948+Polarfoxxx@users.noreply.github.com>
2024-03-09 16:45:41 +0100

    Update NewEvent.tsx

--- client/src/module/CalendarModule/Components/NewEvent/NewEvent.tsx
+++ client/src/module/CalendarModule/Components/NewEvent/NewEvent.tsx
@@ -32,7 +32,11 @@ function NewEvent(props: Type_for_NewEvent): JSX.Element {
     const [selectDate, setSelectDate] = React.useState<number>();
     const [startDate, setStartDate] = React.useState<any>(new Date());
     const [allEvents, setAllEvents] = React.useState(events);
+const { handleSubmit, reset } = useInputValue()
 
+  const submit = (v: TypeForInputsObject["v"]): void => {
+    console.log(v);
+  };
     React.useEffect(() => {
         console.log(props.slotInfo.start.toLocaleDateString());
         setSelectDate(props.slotInfo.start.getTimezoneOffset())
@@ -42,32 +46,40 @@ function NewEvent(props: Type_for_NewEvent): JSX.Element {
     const handleClickRemoveWindowEvent = (): void => {
         props.setNewEventContent(null)
     }
-
+const hadleCreateNewEvent = () => {
+    
+}
     return (
         <div
             onClick={handleClickRemoveWindowEvent}
             className=" fixed w-screen h-screen bg-black bg-opacity-20 z-40 ">
-            <div className=" w-4/6 h-72 absolute rounded-3xl m-auto left-0 right-0 top-0 bottom-0 bg-black z-50">
+            <form 
+                onSubmit={(e) => handleSubmit(e, submit)}
+                className=" w-4/6 h-72 absolute rounded-3xl m-auto left-0 right-0 top-0 bottom-0 bg-black z-50">
                 <input
+                    name="tittle"
                     type="text"
                     placeholder="Add Title"
                     style={{ width: "20%", marginRight: "10px" }}
                     value={newEvent.title}
                     onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
                 <DatePicker
+                    name="startDate"
                     placeholderText="Start Date"
                     selected={newEvent.start}
                     onChange={(start) => setNewEvent({ ...newEvent, start })} />
                 <DatePicker
+                    name="endDate"
                     placeholderText="End Date"
                     selected={newEvent.end}
                     onChange={(end) => setNewEvent({ ...newEvent, end })} />
                 <h1>{selectDate}</h1>
                 <button
+                    onClick={submit}
                     style={{ marginTop: "10px" }}>
                     Add Event
                 </button>
-            </div>
+            </form>
         </div>
 
     );
