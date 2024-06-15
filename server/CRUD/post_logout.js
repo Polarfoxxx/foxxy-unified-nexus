c9872b7e4fd618c9fe1aa1b0a5f2dc9a81549fce
polar_foxx <suchovsky.michal@gmail.com>
2024-06-15 07:53:29 +0200

    test 6

--- server/CRUD/post_logout.js
+++ server/CRUD/post_logout.js
@@ -3,19 +3,25 @@ const router = express.Router();
 const User = require("../mongooseDB/mongooseDB");
 
 router.post("/user", async (req, res) => {
+    const { username } = req.body;
+    
     try {
         // Hľadanie používateľa 
         const user = await User.findOne({ username });
+
         // Kontrola existencie používateľa 
         if (user) {
             user.login.state = false;
-            user.save()
-            res.status(200).send('logout');
+            await user.save(); // Použij await, aby se počkalo na uložení uživatele
 
+            // Vymazání souborů cookie (pokud používáš cookies pro autentizaci)
+            res.clearCookie(username, { httpOnly: true, sameSite: 'None', secure: true });
+            res.status(200).send('logout');
         } else {
             res.status(401).json({ message: "The user does not exist" });
         }
     } catch (error) {
+        console.error(error);
         res.status(500).json({ message: "Internal Server Error" });
     }
 });
