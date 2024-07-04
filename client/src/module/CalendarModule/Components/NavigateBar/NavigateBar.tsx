9bb801d41a91d45acd7490e519206d0887ac30d3
suchovskyMichal <suchovsky.michal@gmail.com>
2024-07-04 15:37:35 +0200

    work on navigatebar

--- client/src/module/CalendarModule/Components/NavigateBar/NavigateBar.tsx
+++ client/src/module/CalendarModule/Components/NavigateBar/NavigateBar.tsx
@@ -5,10 +5,11 @@ function NavigateBar(): JSX.Element {
     const rrr = ["A", "B", "C"]
 
     return (
-        <div className=" w-full h-auto p-2 flex flex-col justify-center items-center">
+        <div className=" w-full h-auto p-2 flex flex-col justify-center items-center gap-4">
             {
                 rrr.map((item, key) =>
                     <div
+                    className=" w-[40px] h-[40px] bg-black text-white rounded-[50%] flex items-center justify-center"
                         key={key}>
                         <h1> {item}</h1>
                     </div>
